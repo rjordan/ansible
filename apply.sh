394220c79c55529dc0a6a877f47926984a14689e
Richard Kent Jordan <rjordan@pobox.com>
2015-09-09 14:57:03 -0500

    Start moving containers out of ESXi

--- apply.sh
+++ apply.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ansible-playbook codecrusade.yml -Kk --ask-vault-pass -i hosts 
+ansible-playbook codecrusade.yml -Kk --ask-vault-pass -i hosts
 ansible-playbook containers.yml -Kk --ask-vault-pass -i hosts --limit docker
