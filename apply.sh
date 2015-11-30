8ea46a1f42f4b1a192e5a2f0245d0937a9f80458
Richard Kent Jordan <rjordan@pobox.com>
2015-11-30 00:38:54 -0600

    Switched to librarian

--- apply.sh
+++ apply.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ansible-playbook codecrusade.yml -Kk --ask-vault-pass -i hosts
-ansible-playbook containers.yml -Kk --ask-vault-pass -i hosts --limit docker
+ansible-playbook codecrusade.yml -Kk --ask-vault-pass -i hosts --limit megatraveler
+#ansible-playbook containers.yml -Kk --ask-vault-pass -i hosts --limit docker
