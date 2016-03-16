5fbc6db7f1e89e986d211d26f137e558c0152b60
Richard Kent Jordan <rjordan@pobox.com>
2016-03-15 22:38:42 -0500

    Round 1 cleanup and refactor.

--- apply.sh
+++ apply.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 ansible-galaxy install -r requirements.yml -p roles/
-#ansible-playbook update_all.yml -K -i hosts
-ansible-playbook codecrusade.yml -K -i hosts
-ansible-playbook docker.yml -K -i hosts 
-ansible-playbook containers.yml -K --ask-vault-pass -i hosts
-ansible-playbook containers2.yml -K -i hosts
+ansible-playbook codecrusade.yml -K -i hosts --ask-vault-pass
+
+
--- apply_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-playbook test.yml -i hosts -K #--ask-vault-pass
