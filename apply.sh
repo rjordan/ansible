1d6510f11604e2495022f515a852d911e57eed33
Richard Kent Jordan <rjordan@pobox.com>
2016-08-13 16:27:15 -0500

    Updates

--- apply.sh
+++ apply.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ansible-galaxy install -r requirements.yml -p roles/
-ansible-playbook codecrusade.yml -K -i hosts --ask-vault-pass
+ansible-playbook codecrusade.yml -i hosts --ask-vault-pass 
 
 
